2da1f56a887aed363d57e876c3a762ce0a0ca486
PeterMikulecky <pmikule1@asu.edu>
2025-02-15 16:20:46 -0800

    Changed imports from bad 'tda' to new 'giotto-tda'

--- src/delay_embedder.py
+++ src/delay_embedder.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.neighbors import KDTree
 from ripser import ripser
-import tda
+from gtda.time_series import TakensEmbedding  # Correct import
 
 class DelayEmbedding:
     def __init__(self, timeseries, dimension, lag):
@@ -11,7 +11,8 @@ class DelayEmbedding:
         self.embeddings = None
 
     def generate_embedding(self):
-        self.embeddings = tda.delay_embedding(self.timeseries, self.dimension, self.lag)
+        embedding = TakensEmbedding(parameters_type='fixed', time_delay=self.lag, dimension=self.dimension)
+        self.embeddings = embedding.fit_transform(self.timeseries.reshape(-1, 1))
         return self.embeddings
 
     def verify_embedding(self):
