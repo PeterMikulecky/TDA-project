f19f4a6a8f5bccc086a5b37eb6b8ae096a8d067a
PeterMikulecky <pmikule1@asu.edu>
2025-02-18 15:30:17 -0800

    Update main.py with array conversion and progress messages

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+import csv
 import data_validator
 import delay_embedder
 import persistence_analyzer
@@ -26,19 +28,40 @@ def main():
     dimension, lag = get_dimension_and_lag()
 
     # Validate the files
+    print("Validating files...\n")
     validator = data_validator.Validation(file1_path, file2_path)
     if not validator.validate_files():
         print("Invalid files")
         return
 
     # Generate time lag embedding for both timeseries
-    timeseries1 = []  # Placeholder for loaded timeseries data
-    timeseries2 = []  # Placeholder for loaded timeseries data
+    print("Converting file 1 to array...\n")
+    file1_second_column = []
+    with open (file1_path, 'r') as file1:
+        reader = csv.reader(file1)
+        next(reader) # Skip header row
+        for row in reader:
+            file1_second_column.append(row[1])
+    timeseries1 = np.array(file1_second_column)
+
+    print("Converting file 2 to array...\n")
+    file2_second_column = []
+    with open (file2_path, 'r') as file2:
+        reader = csv.reader(file2)
+        next(reader) # Skip header row
+        for row in reader:
+            file2_second_column.append(row[1])
+    timeseries2 = np.array(file2_second_column)
+    
+    print("Delay-embedding timeseries 1...\n")
     embedding1 = delay_embedder.DelayEmbedding(timeseries1, dimension, lag).generate_embedding()
+    print("Delay-embedding timeseries 2...\n")
     embedding2 = delay_embedder.DelayEmbedding(timeseries2, dimension, lag).generate_embedding()
 
     # Generate persistence homology and compare
+    print("Calculating persistence homology...\n")
     persistence_analysis = persistence_analyzer.PersistenceAnalysis(embedding1, embedding2)
+    print("Calculating distance metrics...\n")
     wasserstein_dist, bottleneck_dist = persistence_analysis.compare_persistence_data()
 
     # Visualization
