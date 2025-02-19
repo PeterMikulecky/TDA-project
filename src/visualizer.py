717d001628c323f2e2e81e7785f327a3784bf95a
PeterMikulecky <pmikule1@asu.edu>
2025-02-18 17:13:08 -0800

    Update to save image files as oppose to try to display from CLI

--- src/visualizer.py
+++ src/visualizer.py
@@ -2,14 +2,21 @@ import matplotlib.pyplot as plt
 
 class Visualization:
     def __init__(self):
-        pass
+        self.point_cloud_counter = 0  # Counter for point cloud plots
+        self.persistence_homology_counter = 0  # Counter for persistence homology plots
 
     def plot_point_cloud(self, point_cloud):
         # Generate plots of the point clouds
         plt.figure()
         plt.scatter(point_cloud[:, 0], point_cloud[:, 1])
         plt.title("Point Cloud")
-        plt.show()
+        
+        # Generate a unique file name
+        filename = f'point_cloud_{self.point_cloud_counter}.png'
+        plt.savefig(filename)  # Save the plot as an image file
+        plt.close()  # Close the figure
+        
+        self.point_cloud_counter += 1  # Increment the counter
 
     def plot_persistence_homology(self, persistence_data):
         # Generate persistence homology plots
@@ -19,11 +26,18 @@ class Visualization:
             plt.title(f"Persistence Diagram (dim {dim})")
             plt.xlabel("Birth")
             plt.ylabel("Death")
-            plt.show()
+
+            # Generate a unique file name for each diagram
+            filename = f'persistence_diagram_dim_{dim}_{self.persistence_homology_counter}.png'
+            plt.savefig(filename)  # Save the plot as an image file
+            plt.close()  # Close the figure
+            
+            self.persistence_homology_counter += 1  # Increment the counter
 
     def plot_distances(self, wasserstein_dist, bottleneck_dist):
         # Plot Wasserstein and Bottleneck distances
         plt.figure()
         plt.bar(["Wasserstein Distance", "Bottleneck Distance"], [wasserstein_dist, bottleneck_dist])
         plt.title("Persistence Diagram Distances")
-        plt.show()
+        plt.savefig('distances.png')  # Save the plot as an image file
+        plt.close()  # Close the figure
