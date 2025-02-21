b3c2575466e16d90099561820d1e2b8b726a1069
PeterMikulecky <pmikule1@asu.edu>
2025-02-20 22:17:02 -0800

    Switched out Bottleneck distance for StdDev of lifetimes metric

--- src/persistence_analyzer.py
+++ src/persistence_analyzer.py
@@ -14,16 +14,18 @@ class PersistenceAnalysis:
         return diagrams
 
     def compute_wasserstein_distance(self, diagrams1, diagrams2):
-        # Flatten diagrams for input into persim
         diagrams1_flat = np.vstack(diagrams1)
         diagrams2_flat = np.vstack(diagrams2)
         return persim.wasserstein(diagrams1_flat, diagrams2_flat, matching=False)
 
-    def compute_bottleneck_distance(self, diagrams1, diagrams2):
-        # Flatten diagrams for input into persim
-        diagrams1_flat = np.vstack(diagrams1)
-        diagrams2_flat = np.vstack(diagrams2)
-        return persim.bottleneck(diagrams1_flat, diagrams2_flat, matching=False)
+    def compute_std_lifetimes(self, diagrams):
+        lifetimes = []
+        for dgm in diagrams:
+            if len(dgm) > 0:
+                lifetime = dgm[:, 1] - dgm[:, 0]
+                finite_lifetimes = lifetime[np.isfinite(lifetime)]
+                lifetimes.extend(finite_lifetimes)
+        return np.std(lifetimes)
 
     def compare_persistence_data(self):
         print("Working on persistence diagram for timeseries 1...\n")
@@ -32,7 +34,9 @@ class PersistenceAnalysis:
         self.diagrams2 = self.generate_persistence_homology(self.point_cloud2)
         print("Calculating Wasserstein distance. UserWarnings are expected. This could take a few minutes...\n")
         wasserstein_dist = self.compute_wasserstein_distance(self.diagrams1, self.diagrams2)
-        print("Calculating Bottleneck distance. UserWarnings are expected. This could take a few minutes...\n")
-        bottleneck_dist = self.compute_bottleneck_distance(self.diagrams1, self.diagrams2)
+        print("Calculating Standard Deviation of Lifetimes for timeseries 1...\n")
+        std_lifetimes1 = self.compute_std_lifetimes(self.diagrams1)
+        print("Calculating Standard Deviation of Lifetimes for timeseries 2...\n")
+        std_lifetimes2 = self.compute_std_lifetimes(self.diagrams2)
 
-        return wasserstein_dist, bottleneck_dist
+        return wasserstein_dist, std_lifetimes1, std_lifetimes2
